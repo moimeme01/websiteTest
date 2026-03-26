d7b4b43e6cc7cfdba57c19ca93d4b8e4ee8f0e1f
thibaultvanni <tibo.vanni@gmail.com>
2026-03-26 11:54:51 +0100

    removed main.py. Added subpages from Portfolio and some buttons from it.

---
+++ portfolio/src/pages/Famille.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import heroImg from '../assets/hero.png'
+
+import '../App.css'
+
+
+function Famille(){ 
+    return(
+        <>
+            <section id="center">
+                <div className="hero">
+                    <img src={heroImg} className="base" width="170" height="179" alt="" />
+                    <img src={reactLogo} className="framework" alt="React logo" />
+                    <img src={viteLogo} className="vite" alt="Vite logo" />
+                </div>
+                <div>
+                    <h4>Si tu atterris ici c'est que tu fais pasrtie de ma famille. (ou que t'es un bon hacker)</h4>
+                    <p>
+                    
+                    </p>
+                </div>
+                
+            </section>
+        </>
+    )
+};
+
+export default Famille;
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,132 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import heroImg from '../assets/hero.png'
+
+import '../App.css'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="./icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="./icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with me</h2>
+          <p>Join me on my social networks</p>
+          <ul>
+            <li>
+              <a href="https://github.com/moimeme01" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="./icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/thibault-vanni" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="./icons.svg#linkedin-icon"></use>
+                </svg>
+                Linkedin
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="./icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+
+      <section className="navigation-buttons">
+        <Link to="/projets">
+            <button className="btn-projets">Voir mes Projets</button>
+        </Link>
+        <Link to="/famille">
+            <button className="btn-famille">Ma Famille</button>
+        </Link>
+      </section>  
+    </>
+  )
+}
+
+export default Home;
