19fe2cb9965d894a8b873987165108926327cacc
thibaultvanni <tibo.vanni@gmail.com>
2026-08-08 18:12:28 +0200

    hotfix: fixed the settings.py

--- studUPMock/backend/settings.py
+++ studUPMock/backend/settings.py
@@ -1,8 +1,11 @@
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="/Users/thibaultvanni/Code Projects/websiteTest/studUPMock/.env", env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding='utf-8',
+        extra="ignore",
+        )
 
-    DATABASE_URL: str = Field(init=False)
+    DATABASE_URL: str
     
