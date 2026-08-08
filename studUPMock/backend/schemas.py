2fd8c62c255d76decb3f6abc146c0b7bed148d7c
thibaultvanni <tibo.vanni@gmail.com>
2026-08-08 19:54:29 +0200

    fix: test d'un fix

--- studUPMock/backend/schemas.py
+++ studUPMock/backend/schemas.py
@@ -2,11 +2,9 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class UserSchema(BaseModel):
-    id: int
     username: str
     password: str
-    created_at: datetime
-    
+
 class UserPublic(BaseModel):
     id: int
     username: str
