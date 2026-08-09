1a38e51d80e1aeee649f70d48c2d77cd960e3643
thibaultvanni <tibo.vanni@gmail.com>
2026-08-09 20:03:32 +0200

    fix: fixed the error in the import

--- studUPMock/src/main.jsx
+++ studUPMock/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import {AuthProvider} from './context/AuthProvider'
 import Register from './Register'
 import Login from './Login'
-import WELCOMEHOME from '/Welcomehome'
+import WELCOMEHOME from './Welcomehome'
 
 
 createRoot(document.getElementById('root')).render(
