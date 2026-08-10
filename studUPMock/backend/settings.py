fedcb9295a85fd7c6708b3fd0838c2225376824b
thibaultvanni <tibo.vanni@gmail.com>
2026-08-10 12:15:29 +0200

    fix: some errors with the DB

--- studUPMock/backend/settings.py
+++ studUPMock/backend/settings.py
@@ -1,13 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-
     model_config = SettingsConfigDict(
         env_file=".env", 
         env_file_encoding='utf-8',
         extra="ignore",
         )
-
+    DATABASE_URL: str = Field(init = False)
     
     
